cb99341011598b4152cfb6fa811f80acf0c5e82f
Simon Inns <simoninns@fastmail.com>
2025-12-11 20:38:59 +0100

    25.1 first test version of the new FPGA FIFO code

--- DE0-NANO/DomesdayDuplicator/statusLED.v
+++ DE0-NANO/DomesdayDuplicator/statusLED.v
@@ -22,25 +22,26 @@ reg [31:0] timer;
 reg direction;
 reg [3:0] position; // 4-bit value 0-15
 
-always @ (posedge clock) begin
+
+always @ (posedge clock, negedge nReset) begin
 	if (!nReset) begin
 		leds <= 8'b00000001;
-		timer <= 32'd0;
+		timer <= 16'd0;
 		direction <= 1'b1;
 		position <= 4'd0;
 	end else begin
 		timer <= timer + 32'd1;
 		// Wait for the timer to elapse before updating LEDs
-		if (timer >= 32'd3000000) begin
+		if (timer >= 32'd4000000) begin
 			case(position)
-				4'd0:leds <= 8'b00000001;
-				4'd1:leds <= 8'b00000010;
-				4'd2:leds <= 8'b00000100;
-				4'd3:leds <= 8'b00001000;
-				4'd4:leds <= 8'b00010000;
-				4'd5:leds <= 8'b00100000;
-				4'd6:leds <= 8'b01000000;
-				4'd7:leds <= 8'b10000000;
+				4'd0:leds <= 8'b11110000;
+				4'd1:leds <= 8'b00001111;
+				4'd2:leds <= 8'b11000000;
+				4'd3:leds <= 8'b00000011;
+				4'd4:leds <= 8'b00111100;
+				4'd5:leds <= 8'b11000011;
+				4'd6:leds <= 8'b11111111;
+				4'd7:leds <= 8'b00000000;
 			endcase
 		
 			if (direction) begin
